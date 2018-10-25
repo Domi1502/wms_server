9e414bf316c82ac4d151d7d2db139b252b3eeebd
Domi1502 <stanik.d62@gmail.com>
2018-10-25 09:15:38 +0200

    update

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
 console.log("ahoj");
+var express = require("express");
+var path = require("path");
+var server = new express();
+
+var PORT = 3030;
+server.get('/A', function (request, response) {
+console.log(request.query);
+response.send(request.query);
+
+})
+server.listen(PORT, function() {
+    console.log("Server listening on port " + PORT + "!")
+});
+
