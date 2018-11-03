46a5727aec93bd73f744dce5886f9f5ea799c2ed
Domi1502 <stanik.d62@gmail.com>
2018-11-03 21:35:01 +0100

    update

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 console.log("ahoj");
 var express = require("express");
 var path = require("path");
-var server = new express();
+var server = express();
 
-var PORT = 3030;
-server.get('/A', function (request, response) {
-console.log(request.query);
-response.send(request.query);
+var PORT = 3045;
 
+server.get('/wms', function (request, response) {
+    var params=request.query;
+    console.log(params);
+    if(params.SERVICE ==="WMS" && params.REQUEST === 'GetCapabilities') {
+        response.sendFile(path.join(__dirname , 'nase_vrstvy.xml'))
+     }   else if (params.service === "wms" && params.request === 'GetMap') {
+            console.log('idem robit get map')
+    }   else {
+    response.send('nepodporovana metoda')
+}
 })
 server.listen(PORT, function() {
     console.log("Server listening on port " + PORT + "!")
